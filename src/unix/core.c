2e3004f0f90a6df642e22c03fcb488fc1dbc4212
louistru <louistru@hotmial.ocm>
2024-04-30 19:30:58 +0800

    up: note commentary

--- src/unix/core.c
+++ src/unix/core.c
@@ -382,8 +382,8 @@ int uv_run(uv_loop_t* loop, uv_run_mode mode) {
     if ((mode == UV_RUN_ONCE && !ran_pending) || mode == UV_RUN_DEFAULT)
       timeout = uv_backend_timeout(loop);
 
-		if (timeout == -1) {
-			// Check tickets immediately when there are no more asynchronous tasks
+		if (timeout != 0) {
+			// If there is a waiting time, perform a check first
 			uv__run_check(loop);
 		}
     uv__io_poll(loop, timeout);
